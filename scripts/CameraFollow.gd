0ac529aa2b1b9f54cdc1a16cd35588abc7059ff0
MKoltisko <mkoltisko888@gmail.com>
2021-10-16 01:15:43 -0400

    camera follows behind ship with orientation

--- scripts/CameraFollow.gd
+++ scripts/CameraFollow.gd
@@ -1,10 +1,16 @@
 extends Camera
 
-onready var Ship = get_parent_spatial()
-
+onready var Body = get_parent_spatial().get_child(0)
+var initial_rotation
+var camera_to_body
+var radius
 func _ready():
-	pass
+	initial_rotation = rotation.y
+	camera_to_body = Vector3(translation - Body.translation)
+	camera_to_body.y = 0 
+	radius = camera_to_body.length()
 	
 func _physics_process(delta):
-	pass
-
+	rotation.y = initial_rotation + Body.rotation.y
+	translation.x = radius * sin(rotation.y)
+	translation.z = radius * cos(rotation.y)
