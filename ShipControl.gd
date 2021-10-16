8d2c7cd47b078b3f8f6ef8c52dcf2436a02ea8c6
MKoltisko <mkoltisko888@gmail.com>
2021-10-15 21:55:54 -0400

    zerod ship direction and control

--- ShipControl.gd
+++ ShipControl.gd
@@ -1,16 +1,15 @@
 extends KinematicBody
 
 onready var Body = get_node("Body")
-var start_angle = 0
 var forward_speed = 0
 var speed_step = 0.1
 var motion = Vector3.ZERO;
 
 func _ready():
-	start_angle = Body.rotation_degrees.y
 	pass
 
 func _physics_process(delta):
+	
 	if Input.is_action_pressed("ui_left"):
 		Body.rotate_y(-0.01)
 	if Input.is_action_pressed("ui_right"):
@@ -19,10 +18,10 @@ func _physics_process(delta):
 		forward_speed += speed_step
 	if Input.is_action_pressed("ui_down"):
 		forward_speed -= speed_step
-	motion.x = forward_speed * cos(Body.rotation_degrees.y - start_angle)
+	print(Body.rotation_degrees.y)
+	motion.x = forward_speed * cos(Body.rotation_degrees.y)
 	motion.y = 0
-	motion.z = forward_speed * cos(Body.rotation_degrees.y - start_angle - 90)
-	print(motion)
+	motion.z = -forward_speed * sin(Body.rotation_degrees.y)
 	move_and_slide(motion)
 	
 	
